f2147623cb085ba8a74909a5236e5cc9d98e7696
presidoikhu <106991558+presidoikhu@users.noreply.github.com>
2024-09-16 18:13:31 +0100

    Add files via upload

---
+++ newcrypto/script.js
@@ -0,0 +1,13 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        themeToggle.textContent = 'Light Mode';
+    } else {
+        themeToggle.textContent = 'Dark Mode';
+    }
+});
